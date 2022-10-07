388d671771dac112761bb2a36274b57a70bd0340
KotikVacia <vladatkishkin@vk.com>
2022-10-07 17:48:42 +0500

    guide-pdf-3: add ilovepdf integration

--- routes/guide/index.js
+++ routes/guide/index.js
@@ -1,16 +1,16 @@
-import { getTree, getAllUrls } from '../../api/notion-api.js';
-import { createPdf } from '../../domain/pdf.js';
+import { getTree, getAllUrls } from '../../notion-api/notion-api.js';
+import { createPdf } from '../../utils/createPdf.js';
 
-export default async function (fastify, opts) {
-  fastify.get('/:params', async function (request, reply) {
+export default async function (fastify, _) {
+  fastify.get('/:params', async function (request, _) {
     const tree = await getTree();
     const children = tree?.children;
     const allGuidesUrls = getAllUrls(children);
     const guideUrls = allGuidesUrls
       .filter(url => url[0] === request.params.params)
-      .map(arr => arr.join('/'))
-      .map(url => `https://guides.ekaterinburg.design/${url}`)
-    createPdf(guideUrls, 'merge.pdf')
+      .map(arr => `https://guides.ekaterinburg.design/${arr.join('/')}`)
+
+    createPdf(guideUrls, `merge.pdf`)
 
     return {
       guide: guideUrls,
--- routes/root.js
+++ routes/root.js
@@ -1,5 +1,6 @@
-export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
+export default async function (fastify, _) {
+  fastify.get('/', async function (request, _) {
+    console.log(process.env.PDF_PUBLIC_KEY)
     return `
       Микросервис на Fastify для получения pdf файлов руководств
       /guide/:имя_руководства — способ получить pdf файл нужного руководства
