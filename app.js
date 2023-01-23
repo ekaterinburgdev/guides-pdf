6620b601780f5170cfe3c6ad1529bc030dc70d4d
Daniil Gilmutdinov <fregire@mail.ru>
2023-01-23 23:46:42 +0500

    errors logging

--- app.js
+++ app.js
@@ -40,7 +40,19 @@ export default async function (fastify, opts) {
 
   fastify.decorate('notFound', (request, reply) => {
     reply.code(404).type('text/html').send('Not Found')
-  })
+  });
   
   fastify.setNotFoundHandler(fastify.notFound);
+
+  fastify.setErrorHandler(function (error, request, reply) {
+    if (error instanceof Fastify.errorCodes.FST_ERR_BAD_STATUS_CODE) {
+      // Log error
+      this.log.error(error)
+      // Send error response
+      reply.status(500).send({ ok: false })
+    } else {
+      // fastify will use parent error handler to handle this
+      reply.send(error)
+    }
+  })
 }
--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,10 @@
 export default async function (fastify, _) {
   fastify.get('/', async function (request, _) {
-    console.log(process.env.PDF_PUBLIC_KEY)
+    // console.log(process.env.PDF_PUBLIC_KEY)
     return `
       Микросервис на Fastify для получения pdf файлов руководств
-      /guide/:имя_руководства — способ получить pdf файл нужного руководства
+      /guide/:имя_руководства — создание pdf'ки нужного руководства
+      /guides/ - получить список уже существующих pdf руководств
     `
   })
 }
