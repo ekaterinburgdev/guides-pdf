1e003911399bb691708c7cc5d560c963fa213eb7
Daniil Gilmutdinov <fregire@mail.ru>
2023-01-24 23:28:07 +0500

    fixed problem by updating packages

--- utils/createPdf.js
+++ utils/createPdf.js
@@ -7,18 +7,21 @@ export const createPdf = async (urls, filename) => {
       ignoreHTTPSErrors: true,
       args: ['--no-sandbox']
   });
-  const [page] = await browser.pages();
+  const page = await browser.newPage();
   const merger = new PDFMerger();
 
   for (const url of urls) {
     await page.goto(url, {
-      waitUntil: 'networkidle0'
-    }, { timeout: 0 });
-    await merger.add(await page.pdf({
+      waitUntil: 'networkidle0',
+      timeout: 0
+    });
+    const pagePdf = await page.pdf({
       printBackground: true,
-      format: 'a4'
-    }));
+      format: 'a4',
+      timeout: 0
+    });
+    merger.add(pagePdf);
   }
   
-  merger.save(filename);
+  await merger.save(filename);
 }
