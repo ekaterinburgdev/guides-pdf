f914f2700497c6fca405f4c8f8704253d2c4ea2d
Daniil Gilmutdinov <fregire@mail.ru>
2023-01-24 23:51:32 +0500

    some docs corrections

--- routes/root.js
+++ routes/root.js
@@ -3,9 +3,9 @@ export default async function (fastify, _) {
     // console.log(process.env.PDF_PUBLIC_KEY)
     return `
       Микросервис на Fastify для получения pdf файлов руководств
-      /guide/:имя_руководства — создание pdf'ки нужного руководства
-      /guides/ - получить список уже существующих pdf руководств
-      /guides/guide_name.pdf - получить руководство в pdf формате
+      GET /guide/:имя_руководства — создание pdf'ки нужного руководства
+      GET /guides/ - получить список уже существующих pdf руководств ( ответ в формате {"guides": ["guide1.pdf", "guide2.pdf"]} )
+      GET /guides/guide_name.pdf - получить руководство в pdf формате
     `
   })
 }
