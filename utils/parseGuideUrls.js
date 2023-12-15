9ee91b2c9a10bcf5ff3e30d9da85e33711d56d7e
atkvs <vladatkishkin@vk.com>
2023-12-15 22:30:05 +0500

    build(guide-pdf): remove pargeGuideUrl

--- utils/parseGuideUrls.js
+++
@@ -1,8 +0,0 @@
-const parseGuideUrls = async () => {
-    const response = await got('http://guides-api-test.ekaterinburg.design:48700/api/tree').json()
-    for (const i in response) {
-        if (Array.isArray(response[i])) {
-            return response[i].map(i => i.properties.pageUrl.url)
-        };
-    }
-}
